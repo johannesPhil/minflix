e54bdf81acdc55534e9ffc7038c4a60a384705db
johannesPhil <jay3pee@gmail.com>
2019-12-28 00:45:37 +0100

    App Built

--- src/app/signup/user.model.ts
+++ src/app/signup/user.model.ts
@@ -2,19 +2,19 @@ import {required, compare, alpha, minLength, email } from '@rxweb/reactive-form-
 
 export class User{
 
-  @required({message: 'Required'})
+  
   @alpha({message: 'Only Alphabetic characters allowed'})
-  @minLength({value: 3, message: 'Name should contain at lest 3 characters'})
+  @minLength({value: 3, message: 'Name should contain at least 3 characters'})
   name: string;
 
   @email({message: 'Invalid email format'})
   mail: string;
 
-  @required({message: 'Required'})
+  
   @minLength({value: 6, message: 'Password should contain at least 6 characters'})
   password: string;
 
-  @required({message: 'Required'})
+  
   @compare({fieldName: 'password', message: 'Passwords do not match'})
   confirmPassword: string;
 
